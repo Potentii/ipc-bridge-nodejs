e22e27f1454f56fe2d8067f6c701501ddbb2b331
Guilherme Reginaldo Ruella <potentii@gmail.com>
2018-04-17 16:50:42 -0300

    JSDoc
    - Added JSDoc

--- libs/master/api/ipc-master.js
+++ libs/master/api/ipc-master.js
@@ -1,35 +1,90 @@
+// *Requesting the needed modules:
 const uuid = require('uuid');
 const IPCChildProcess = require('../ipc-child-process');
 const OutgoingMessage = require('../../types/outgoing-message');
 
 
 
+/**
+ * The master side of the IPC bridge
+ * This is the entity responsible to start and manage a new worker sub-process
+ * with which it'll stablish a communication line (also called IPC bridge)
+ * using their STDIO channels to exchange messages between them.
+ * @class
+ */
 module.exports = class IPCMaster{
+
+   /**
+    * Creates the manager of a sub-process, with which it'll exchange messages.
+    * Each master instance is responsible for only one worker sub-process.
+    * Note that the provided command must start a process that implements this
+    * same protocol, in order for the IPC bridge to work properly.
+    * @constructor
+    * @param {String} command_name The main command to spawn the sub-process
+    * @param {String[]} [args]     The aditional arguments for the mais command
+    */
    constructor(command_name, args){
+      // *Creating the unit that will handle the sub-process activities:
       this._child = new IPCChildProcess(command_name, args);
    }
 
-   start(){
+
+   /**
+    * Spawns and starts to manage the sub-process.
+    * If the sub-process have already been started, this method has no effect.
+    * @return {Promise} Resolves when the sub-process gets ready to listen for
+    *                   requests
+    */
+   async start(){
+      // *Exiting this method if the worker already got started:
+      if(this._child.started())
+         return;
+
+      // *Registering a listener for errors:
       this._child.on('error', err => {
          console.error(err);
       });
 
-      return this._child.start();
+      // *Returning the sub-process start promise:
+      return await this._child.start();
    }
 
-   stop(){
-      return this._child.stop();
+
+   /**
+    * Kills the sub-process.
+    * If the sub-process is already stopped, this method has no effect.
+    * @return {Promise} Resolves when the sub-process gets killed
+    */
+   async stop(){
+      // *Exiting this method if the worker is already stopped:
+      if(!this._child.started())
+         return;
+
+      // *Returning the stopping promise:
+      return await this._child.stop();
    }
 
+
+   /**
+    * Sends a new message to the worker sub-process.
+    * The sub-process will be started if it isn't already.
+    * @param  {Function} requestBuilder The function that will receive the
+    *                                   request object and change it to be sent
+    *                                   to the worker.
+    * @return {Promise}                 Resolves into the received response
+    */
    async send(requestBuilder){
-
+      // *Starting the sub-process if it's not already started
       if(!this._child.started())
          await this._child.start();
 
+      // *Generating a pseudo-random message identifier:
       const message_id = uuid.v4();
 
+      // *Initializing the request object:
       const req = new OutgoingMessage(message_id);
 
+      // *Executing the request decoration function, if it have been provided:
       if(typeof requestBuilder === 'function')
          requestBuilder(req);
 
